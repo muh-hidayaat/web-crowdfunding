4293e403b490e162385c7a91ce5af29e9db3fee8
hdyt <muh.hidayaat24@gmail.com>
2025-04-25 19:28:44 +0800

    first commit

---
+++ main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"crowdfund/users"
+	"log"
+
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+)
+
+func main() {
+	// refer https://github.com/go-sql-driver/mysql#dsn-data-source-name for details
+	dsn := "root:root@tcp(127.0.0.1:3306)/db_crowdfunding?charset=utf8mb4&parseTime=True&loc=Local"
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+	
+
+	userRepository := users.NewRepository(db)
+	user := users.User{
+		Name: "Test simpan",
+	}
+
+	userRepository.Save(user)
+}
